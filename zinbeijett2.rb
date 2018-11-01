2043c995918cf4130a25b2ebe92fb02fb9a9fe14
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-11-01 22:59:43 +0900

    Update

--- zinbeijett2.rb
+++ zinbeijett2.rb
@@ -4,7 +4,7 @@ class Zinbeijett2 < Formula
   
   homepage "https://github.com/takkii/zinbeijett2"
   url "https://github.com/takkii/zinbeijett2/archive/2.0.6.tar.gz"
-  sha256 ""
+  sha256 "6082c801042a8d7b658bbf776e153a639d1223f0dea12d321c115db870217114"
   head "https://github.com/takkii/zinbeijett2.git"
   version "2.0.0"
 
