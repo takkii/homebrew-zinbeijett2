9743c3b9042930262a65e2b574198cbb1bb7c481
Takayuki Kamiyama <karuma.reason@gmail.com>
2018-11-06 23:30:49 +0900

    Update

--- zinbeijett2.rb
+++ zinbeijett2.rb
@@ -3,10 +3,10 @@ require "formula"
 class Zinbeijett2 < Formula
   
   homepage "https://github.com/takkii/zinbeijett2"
-  url "https://github.com/takkii/zinbeijett2/archive/2.1.0_07.tar.gz"
+  url "https://github.com/takkii/zinbeijett2/archive/2.1.1.tar.gz"
   sha256 ""
   head "https://github.com/takkii/zinbeijett2.git"
-  version "2.1.0_07"
+  version "2.1.1"
 
   def install
    bin.install Dir['bin/*']
